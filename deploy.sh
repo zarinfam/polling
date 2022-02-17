964dc092678da80bf58a52ed9ceadd0913c0fa3d
Saeed Zarinfam <zarinfam.s@gmail.com>
2022-02-17 09:06:56 +0100

    configured skaffold

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # replace image version using kustomize or mannully in k8s yamel file for api and frontend
 # minikube addons enable ingress
 # run kubectl apply -k k8s
+# minikube ip
 
 
 cd api
@@ -14,3 +15,8 @@ cd frontend
 docker build -t zarinfam/polling-frontend:0.0.1 .
 docker push zarinfam/polling-frontend:0.0.1
 cd ..
+
+------local deveopment using skaffold--------------
+skaffold config set --global local-cluster true
+skaffold init
+skaffold dev
